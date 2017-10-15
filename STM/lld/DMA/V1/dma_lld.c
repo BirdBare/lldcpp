208909c8c17f70e59563de648ff278aeafdc468c
Brad <BradleyBare@gmail.com>
2017-10-15 11:34:37 -0500

    trying something

--- STM/lld/DMA/V1/dma_lld.c
+++ STM/lld/DMA/V1/dma_lld.c
@@ -10,61 +10,27 @@
 
 
 struct DmaObject
-	DMA1_STREAM0_OBJECT = {{0x30,21},0,0,DMA1_Stream0},
-	DMA1_STREAM1_OBJECT = {{0x30,21},0,6,DMA1_Stream1},
-	DMA1_STREAM2_OBJECT = {{0x30,21},0,16,DMA1_Stream2},
-	DMA1_STREAM3_OBJECT = {{0x30,21},0,22,DMA1_Stream3},
-	DMA1_STREAM4_OBJECT = {{0x30,21},4,0,DMA1_Stream4},
-	DMA1_STREAM5_OBJECT = {{0x30,21},4,6,DMA1_Stream5},
-	DMA1_STREAM6_OBJECT = {{0x30,21},4,16,DMA1_Stream6},
-	DMA1_STREAM7_OBJECT = {{0x30,21},4,22,DMA1_Stream7},
+	DMA1S0_OBJECT = {{0x30,21},0,0,DMA1_Stream0},
+	DMA1S1_OBJECT = {{0x30,21},0,6,DMA1_Stream1},
+	DMA1S2_OBJECT = {{0x30,21},0,16,DMA1_Stream2},
+	DMA1S3_OBJECT = {{0x30,21},0,22,DMA1_Stream3},
+	DMA1S4_OBJECT = {{0x30,21},4,0,DMA1_Stream4},
+	DMA1S5_OBJECT = {{0x30,21},4,6,DMA1_Stream5},
+	DMA1S6_OBJECT = {{0x30,21},4,16,DMA1_Stream6},
+	DMA1S7_OBJECT = {{0x30,21},4,22,DMA1_Stream7},
 
-	DMA2_STREAM0_OBJECT = {{0x30,22},0,0,DMA2_Stream0},
-	DMA2_STREAM1_OBJECT = {{0x30,22},0,6,DMA2_Stream1},
-	DMA2_STREAM2_OBJECT = {{0x30,22},0,16,DMA2_Stream2},
-	DMA2_STREAM3_OBJECT = {{0x30,22},0,22,DMA2_Stream3},
-	DMA2_STREAM4_OBJECT = {{0x30,22},4,0,DMA2_Stream4},
-	DMA2_STREAM5_OBJECT = {{0x30,22},4,6,DMA2_Stream5},
-	DMA2_STREAM6_OBJECT = {{0x30,22},4,16,DMA2_Stream6},
-	DMA2_STREAM7_OBJECT = {{0x30,22},4,22,DMA2_Stream7};
-
-
+	DMA2S0_OBJECT = {{0x30,22},0,0,DMA2_Stream0},
+	DMA2S1_OBJECT = {{0x30,22},0,6,DMA2_Stream1},
+	DMA2S2_OBJECT = {{0x30,22},0,16,DMA2_Stream2},
+	DMA2S3_OBJECT = {{0x30,22},0,22,DMA2_Stream3},
+	DMA2S4_OBJECT = {{0x30,22},4,0,DMA2_Stream4},
+	DMA2S5_OBJECT = {{0x30,22},4,6,DMA2_Stream5},
+	DMA2S6_OBJECT = {{0x30,22},4,16,DMA2_Stream6},
+	DMA2S7_OBJECT = {{0x30,22},4,22,DMA2_Stream7};
 
 
-uint32_t DmaConfig(struct DmaObject *dma_stream_object, struct DmaConfig *dma_config)
-{
-	volatile DMA_Stream_TypeDef *dma_stream = dma_stream_object->dma_stream;
-	//get dma_stream
 
-	if((dma_stream->CR & DMA_SxCR_EN) == 1)
-	{
-		return 1;
-		//if the stream is enabled then we cannot config
-	}
 
-	DmaClearFlags(dma_stream_object,0b111101);
-	//reset flags in the dma
-
-	dma_stream->NDTR = dma_config->ndtr;
-	dma_stream->PAR = dma_config->par;
-	dma_stream->M0AR = dma_config->m0ar;
-	dma_stream->M1AR = dma_config->m1ar;
-	dma_stream->FCR = dma_config->fcr;
-	dma_stream->CR = dma_config->cr | DMA_SxCR_EN;
-	//configure and enable the dma stream
-
-	return 0;
-}
-
-void DmaEnable(struct DmaObject *dma_stream_object)
-{
-	dma_stream_object->dma_stream->CR |= DMA_SxCR_EN;
-}
-
-void DmaDisable(struct DmaObject *dma_stream_object)
-{
-	dma_stream_object->dma_stream->CR &= ~DMA_SxCR_EN;
-}
 
 
 //******************************************************************************
@@ -75,47 +41,47 @@ void DmaDisable(struct DmaObject *dma_stream_object)
 
 ALWAYS_INLINE void DMA_STREAM_HANDLER(struct DmaObject *dma_stream_object)
 {
-	EventSignalFlags(&dma_stream_object->event, DmaGetFlags(dma_stream_object));	
+return;
 }
 
 #ifdef DMA1
 void DMA1_Stream0_IRQHandler(void) 
 {
-	DMA_STREAM_HANDLER(&DMA1_STREAM0_OBJECT);
+	DMA_STREAM_HANDLER(&DMA1S0_OBJECT);
 }
 void DMA1_Stream1_IRQHandler(void) 
 {
-	DMA_STREAM_HANDLER(&DMA1_STREAM1_OBJECT);
+	DMA_STREAM_HANDLER(&DMA1S1_OBJECT);
 }
 
 void DMA1_Stream2_IRQHandler(void) 
 {
-	DMA_STREAM_HANDLER(&DMA1_STREAM2_OBJECT);
+	DMA_STREAM_HANDLER(&DMA1S2_OBJECT);
 }
 
 void DMA1_Stream3_IRQHandler(void) 
 {
-	DMA_STREAM_HANDLER(&DMA1_STREAM3_OBJECT);
+	DMA_STREAM_HANDLER(&DMA1S3_OBJECT);
 }
 
 void DMA1_Stream4_IRQHandler(void) 
 {
-	DMA_STREAM_HANDLER(&DMA1_STREAM4_OBJECT);
+	DMA_STREAM_HANDLER(&DMA1S4_OBJECT);
 }
 
 void DMA1_Stream5_IRQHandler(void) 
 {
-	DMA_STREAM_HANDLER(&DMA1_STREAM5_OBJECT);
+	DMA_STREAM_HANDLER(&DMA1S5_OBJECT);
 }
 
 void DMA1_Stream6_IRQHandler(void) 
 {
-	DMA_STREAM_HANDLER(&DMA1_STREAM6_OBJECT);
+	DMA_STREAM_HANDLER(&DMA1S6_OBJECT);
 }
 
 void DMA1_Stream7_IRQHandler(void) 
 {
-	DMA_STREAM_HANDLER(&DMA1_STREAM7_OBJECT);
+	DMA_STREAM_HANDLER(&DMA1S7_OBJECT);
 }
 
 #endif
@@ -123,42 +89,42 @@ void DMA1_Stream7_IRQHandler(void)
 #ifdef DMA2
 void DMA2_Stream0_IRQHandler(void) 
 {
-	DMA_STREAM_HANDLER(&DMA2_STREAM1_OBJECT);
+	DMA_STREAM_HANDLER(&DMA2S1_OBJECT);
 }
 
 void DMA2_Stream1_IRQHandler(void) 
 {
-	DMA_STREAM_HANDLER(&DMA2_STREAM2_OBJECT);
+	DMA_STREAM_HANDLER(&DMA2S2_OBJECT);
 }
 
 void DMA2_Stream2_IRQHandler(void) 
 {
-	DMA_STREAM_HANDLER(&DMA2_STREAM3_OBJECT);
+	DMA_STREAM_HANDLER(&DMA2S3_OBJECT);
 }
 
 void DMA2_Stream3_IRQHandler(void) 
 {
-	DMA_STREAM_HANDLER(&DMA2_STREAM4_OBJECT);
+	DMA_STREAM_HANDLER(&DMA2S4_OBJECT);
 }
 
 void DMA2_Stream4_IRQHandler(void) 
 {
-	DMA_STREAM_HANDLER(&DMA2_STREAM5_OBJECT);
+	DMA_STREAM_HANDLER(&DMA2S5_OBJECT);
 }
 
 void DMA2_Stream5_IRQHandler(void) 
 {
-	DMA_STREAM_HANDLER(&DMA2_STREAM6_OBJECT);
+	DMA_STREAM_HANDLER(&DMA2S6_OBJECT);
 }
 
 void DMA2_Stream6_IRQHandler(void) 
 {
-	DMA_STREAM_HANDLER(&DMA2_STREAM7_OBJECT);
+	DMA_STREAM_HANDLER(&DMA2S7_OBJECT);
 }
 
 void DMA2_Stream7_IRQHandler(void) 
 {
-	DMA_STREAM_HANDLER(&DMA1_STREAM0_OBJECT);
+	DMA_STREAM_HANDLER(&DMA2S0_OBJECT);
 }
 //##############################################################################
 
--- STM/lld/DMA/V1/dma_lld.h
+++ STM/lld/DMA/V1/dma_lld.h
@@ -11,8 +11,6 @@
 
 #include "board.h"
 #include "rcc_lld.h"
-#include "event.h"
-
 
 //to get DMA Base address. Take DMA_Stream & ~255.
 
@@ -24,132 +22,64 @@ struct DmaObject
 	const uint8_t flag_offset;
 
 	volatile DMA_Stream_TypeDef * const dma_stream;
-
-	volatile struct Mutex *mutex;
-
-	struct EventSource event;
 }; 
 
 extern struct DmaObject 
-	DMA1_STREAM0_OBJECT,
-	DMA1_STREAM1_OBJECT,
-	DMA1_STREAM2_OBJECT,
-	DMA1_STREAM3_OBJECT,
-	DMA1_STREAM4_OBJECT,
-	DMA1_STREAM5_OBJECT,
-	DMA1_STREAM6_OBJECT,
-	DMA1_STREAM7_OBJECT,
+	DMA1S0_OBJECT,
+	DMA1S1_OBJECT,
+	DMA1S2_OBJECT,
+	DMA1S3_OBJECT,
+	DMA1S4_OBJECT,
+	DMA1S5_OBJECT,
+	DMA1S6_OBJECT,
+	DMA1S7_OBJECT,
 
-	DMA2_STREAM0_OBJECT,
-	DMA2_STREAM1_OBJECT,
-	DMA2_STREAM2_OBJECT,
-	DMA2_STREAM3_OBJECT,
-	DMA2_STREAM4_OBJECT,
-	DMA2_STREAM6_OBJECT,
-	DMA2_STREAM7_OBJECT;
+	DMA2S0_OBJECT,
+	DMA2S1_OBJECT,
+	DMA2S2_OBJECT,
+	DMA2S3_OBJECT,
+	DMA2S4_OBJECT,
+	DMA2S6_OBJECT,
+	DMA2S7_OBJECT;
 
-struct DmaConfig
+
+
+//two different configuration modes for the dma.
+ALWAYS_INLINE void DmaConfigCR(const struct DmaObject const *dma_object,
+	const uint32_t cr)
 {
-	union
-	{
-		struct
-		{
-			uint32_t cr; //configuration register
-			uint32_t ndtr; //number of data to transfer register
-			uint32_t par; //peripheral address register
-			uint32_t m0ar; //memory 0 address register
-			uint32_t m1ar; //memory 1 address register
-			uint32_t fcr; //fifo control register
-		};
+	dma_object->dma_stream->CR = cr;
+}
 
-		struct
-		{
-			//LSB
-			uint32_t :1; // 
-			uint32_t dmeie:1; // Direct mode error interrupt enable
-			uint32_t teie:1; // Transfer Error Interrupt enable
-			uint32_t htie:1; // Half transfer complete enable
-			uint32_t tcie:1; // Transfer Complete Interrupt enable
-			uint32_t pfctrl:1; // Peripheral Flow Controller select
-#define PFCTRL_DMA 0
-#define PFCTRL_PERIPHERAL 1
+ALWAYS_INLINE void DmaConfigNDTR(const struct DmaObject const *dma_object,
+	const uint32_t number_data_to_transfer)
+{
+	dma_object->dma_stream->NDTR = number_data_to_transfer;
+}
 
-			uint32_t dir:2; //Data transfer Direction
-#define DIR_P2M 0
-#define DIR_M2P 1
-#define DIR_M2M 2
+ALWAYS_INLINE void DmaConfigPAR(const struct DmaObject const *dma_object,
+	const void* peripheral_address)
+{
+	dma_object->dma_stream->PAR = (uint32_t)peripheral_address;
+}
 
-			uint32_t circ:1; //Circular mode enable bit
-			uint32_t pinc:1; //Peripheral address increment enable
-			uint32_t minc:1; //memory address increment enable
-			uint32_t psize:2; //Peripheral data size
-			uint32_t msize:2; //Memory data size
-#define SIZE_BYTE 0
-#define SIZE_HALFWORD 1
-#define SIZE_WORD 2
+ALWAYS_INLINE void DmaConfigM0AR(const struct DmaObject const *dma_object,
+	const void* memory_address)
+{
+	dma_object->dma_stream->M0AR = (uint32_t)memory_address;
+}
 
-			uint32_t pincos:1; // Peripheral Increment disable bit
-			uint32_t pl:2; //priority level
-#define PL_LOW 0
-#define PL_MED 1
-#define PL_HIGH 2
-#define PL_VHIGH 3
+ALWAYS_INLINE void DmaConfigM1AR(const struct DmaObject const *dma_object,
+	const void* memory_address)
+{
+	dma_object->dma_stream->M1AR = (uint32_t)memory_address;
+}
 
-			uint32_t dbm:1; //Double buffer mode enable
-			uint32_t ct:1; // current target for double buffer mode
-			uint32_t :1; //
-			uint32_t pburst:2; //Peripheral Burst Length
-			uint32_t mburst:2; //Memory Burst Length
-#define BURST_1 0
-#define BURST_4 1
-#define BURST_8 2
-#define BURST_16 3
-
-			uint32_t chsel:3; //Channel Selection for peripherals
-#define CHSEL_0 0
-#define CHSEL_1 1
-#define CHSEL_2 2
-#define CHSEL_3 3
-#define CHSEL_4 4
-#define CHSEL_5 5
-#define CHSEL_6 6
-#define CHSEL_7 7
-
-			uint32_t :4;
-			//MSB
-
-			//LSB
-			uint32_t :32; //holder for Number of data register	
-			//MSB
-
-			//LSB
-			uint32_t :32; // holder for peripheral address register
-			//MSB
-			//LSB
-			uint32_t :32; //holder for memory 1 address register
-			//MSB
-			//LSB
-			uint32_t :32; //holder for memory 2 address register
-			//MSB
-			//LSB
-			uint32_t fth:2; //fifo threshold size
-#define FTH_14
-#define FTH_24
-#define FTH_34
-#define FTH_44
-
-			uint32_t dmdis:1; //direct mode disable
-			uint32_t :3; //fifo status bits
-			uint32_t :1;
-			uint32_t feie:1; //fifo error interrupt enable
-			uint32_t :24; 
-			//MSB
-			//LSB
-
-			//MSB
-		};
-	};
-};
+ALWAYS_INLINE void DmaConfigFCR(const struct DmaObject const *dma_object,
+	const uint32_t fcr)
+{
+	dma_object->dma_stream->FCR = fcr;
+}
 
 #define DMA_ISR_FEIF 0b0
 #define DMA_ISR_DMEIF 0b001
@@ -158,23 +88,14 @@ struct DmaConfig
 #define DMA_ISR_TCIF 0b000001
 
 
-
-//******************************************************************************
-//	
-//										 Dma Config
-//	
-//******************************************************************************
-uint32_t DmaConfig(struct DmaObject *dma_stream_object, struct DmaConfig *dma_config);
-//##############################################################################
-
 //******************************************************************************
 //	
 //										 Dma enable/disable
 //	
 //******************************************************************************
 
-void DmaEnable(struct DmaObject *dma_stream_object);
-void DmaDisable(struct DmaObject *dma_stream_object);
+void DmaEnable(struct DmaObject *dma_object);
+void DmaDisable(struct DmaObject *dma_object);
 //##############################################################################
 
 //******************************************************************************
