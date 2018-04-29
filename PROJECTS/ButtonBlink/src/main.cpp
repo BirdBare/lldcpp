4ae596a51f7848ee872c7d4403568dc3dcb853f5
BradleyBare <BradleyBare@gmail.com>
2018-04-29 13:02:17 -0500

    Reorganized main for clarity

--- PROJECTS/ButtonBlink/src/main.cpp
+++ PROJECTS/ButtonBlink/src/main.cpp
@@ -13,23 +13,28 @@
 
 void call(void *args)
 {
+args = args;
 }
 
 int main(void)
 {
+	uint32_t dmapins[50] = {0};
+	uint32_t waste[50];
+
+	DmaInterrupt dma_transfer(DMA2S3_HAL);
+	dma_transfer.SetCallback(&call,0);
+	dma_transfer.MemSet(dmapins,GPIO_PIN_12 | GPIO_PIN_14 | GPIO_PIN_15,50);
+	//dma sets correct pins as a test
+
 	SpiSettings spisettings;
 	spisettings.Master(true).DataSize(2);
 
 	SpiDma spi(SPI1_HAL,spisettings);
 
-	uint32_t dmapins[50] = {0};
-
-	DmaSettings dsettings;
-
-	DmaInterrupt dma_transfer(DMA2S3_HAL, dsettings);
-	dma_transfer.SetCallback(&call,0);
-	dma_transfer.MemSet(dmapins,GPIO_PIN_12 | GPIO_PIN_14 | GPIO_PIN_15,50);
-	//dma sets correct pins as a test
+	GpioOutput GPIOD_OUT(
+		GPIOD_HAL, 
+		dmapins[0]);
+	//gpio stuff
 
 	GpioSettings gsettings;
 	gsettings.Alt(GPIO_ALT_5).PuPd(GPIO_PUPD_PD);
@@ -39,15 +44,6 @@ int main(void)
 		GPIO_PIN_5 | GPIO_PIN_7,
 		gsettings);
 	//spi gpio stuff
-
-	
-GpioOutput GPIOD_OUT(
-		GPIOD_HAL, 
-		dmapins[0]);
-	//gpio stuff
-
-uint8_t data = 0b10010010;
-uint32_t waste[50];
 
 while(1)
 {
